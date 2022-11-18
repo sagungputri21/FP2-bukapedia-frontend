4081ee67799b58b4addeab27e93ec53adaa9b8b3
Sagung Putri Nariswari <sagungputri1530@gmail.com>
2022-11-19 01:13:20 +0700

    add all page of admin and user

---
+++ src/pages/admin/RekapPenjualan.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RekapPenjualan = () => {
+  return (
+    <div>Ini halaman rekap penjualan (admin) </div>
+  )
+}
+
+export default RekapPenjualan;
---
+++ src/pages/admin/UpdateStock.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateStockPage = () => {
+  return (
+    <div>Ini halaman update stock (home admin) </div>
+  )
+}
+
+export default UpdateStockPage;
